57277fd7333bc60c52bb4494c2e66414f2b160c0
waad <waadmahmoud2044@gmail.com>
2024-07-16 19:53:33 +0200

    app inti

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:islami_app/SebhaScreen.dart';
+
+void main(){
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Sebhascreen.routeName,
+      routes: {
+        Sebhascreen.routeName:(context)=>Sebhascreen(),
+      },
+    );
+  }
+}
